e6aab6524cf10e24bf721f4d54d947d93a23f99f
Alexandre Pires <c.alexandre.pires@gmail.com>
2023-08-27 16:55:39 +0200

    Minor fix

--- automash.zsh
+++ automash.zsh
@@ -2,19 +2,19 @@
 
 # If we are running the script then we want to install it
 if [[ $0 == $_ ]]; then
-    printf "Running infra-tools installation!\n"
+    printf "Running automash installation!\n"
     
     # Get script directory
     SCRIPTDIR=$(dirname "$(readlink -f "$0")")
     
     # Check if the source line is already added to users bashrc
-    cat ${HOME}/.zshrc | grep -q "source ${SCRIPTDIR}/infra-tools.zsh" && { printf "Already installed! Exiting...\n"; exit 0; }
+    cat ${HOME}/.zshrc | grep -q "source ${SCRIPTDIR}/automash.zsh" && { printf "Already installed! Exiting...\n"; exit 0; }
     
     # Create required folders folder
     mkdir -p ${HOME}/.ssh/keys
     
     # Add source line to users bashrx
-    printf "\n%s\n" "source ${SCRIPTDIR}/infra-tools.zsh" >> ${HOME}/.zshrc
+    printf "\n%s\n" "source ${SCRIPTDIR}/automash.zsh" >> ${HOME}/.zshrc
     printf "Successful installed on your system!\n"
     sudo -u ${USER} -i
 fi
